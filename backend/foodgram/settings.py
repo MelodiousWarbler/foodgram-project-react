a53e43eae84b65ec0ac01d2d3c234126a880e274
MelodiousWarbler <plakotnyuk@gmail.com>
2023-06-07 17:44:04 +0000

    v0.9_d

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'djoser',
     # 'rest_framework_simplejwt',
     'django_filters',
     'recipes.apps.RecipesConfig',
