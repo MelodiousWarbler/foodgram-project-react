f42ec472e99c82fc9466d5abbf3385043a51b9e5
MelodiousWarbler <plakotnyuk@gmail.com>
2023-06-08 09:38:40 +0000

    v1.0_$

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -88,7 +88,8 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-            'NAME': os.getenv('DB_NAME', 'postgres'),
+            # 'NAME': os.getenv('DB_NAME', 'postgres'),
+            'NAME': os.getenv('POSTGRES_DB', 'postgres'),
             'USER': os.getenv('POSTGRES_USER', 'postgres'),
             'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
             'HOST': os.getenv('DB_HOST', 'db'),
--- backend/foodgram/validators.py
+++ backend/foodgram/validators.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 
 
 def validate_username(value):
-    if re.search(r'^[\w.@+-]+\z', value) is None:
+    if re.search(r'^[\w.@+-]+$', value) is None:
         raise ValidationError(
             ('Допустимы буквы латинского алфавита, цифры и символы .@+-'),
             params={'value': value},
