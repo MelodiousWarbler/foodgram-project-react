ada67b5d8598859d3b9c476c92f5b9c1d1feb3ae
MelodiousWarbler <plakotnyuk@gmail.com>
2023-06-08 19:50:55 +0000

    v1.2_serial

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls', namespace='api'))
+    path('api/', include(('api.urls', 'api'), namespace='api'))
 ]
 
 urlpatterns += [
