a8c6ca5b3adb0dbed7de070783041328af8cdc64
MelodiousWarbler <plakotnyuk@gmail.com>
2023-06-15 10:49:02 +0000

    v1.4_mod

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -89,7 +89,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-            'NAME': os.getenv('DB_NAME', 'postgres'),
+            # 'NAME': os.getenv('DB_NAME', 'postgres'),
             'NAME': os.getenv('POSTGRES_DB', 'postgres'),
             'USER': os.getenv('POSTGRES_USER', 'postgres'),
             'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
